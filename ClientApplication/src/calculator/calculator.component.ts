95bbbc0355d92f0d7b569574322de45b319f11d9
Venura K Komangodage <kaveeshak@gmail.com>
2018-09-03 14:36:41 -0400

    Angular Components.

---
+++ ClientApplication/src/calculator/calculator.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-calculator',
+    templateUrl: './calculator.component.html'
+})
+export class CalculatorComponent {
+
+    number1 = 0;
+    number2 = 0;
+
+    getSumOfNumbers() {
+        
+        return this.number1 + this.number2;
+    }
+}
+
