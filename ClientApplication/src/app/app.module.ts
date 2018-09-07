b34762ae57d5edec86cc77fbd4fca861b59ebefe
Venura K Komangodage <kaveeshak@gmail.com>
2018-09-06 20:03:29 -0400

    Cleaning..

--- ClientApplication/src/app/app.module.ts
+++ ClientApplication/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { InlineComponent } from './inline/inline.component';
+import { SelectorComponent } from './selector/selector.component';
+import { DataBindingComponent } from './dataBinding/dataBinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    InlineComponent
+    InlineComponent, 
+    SelectorComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ ClientApplication/src/app/dataBinding/dataBinding.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-dataBinding',
+    templateUrl: './dataBinding.component.html'
+})
+export class DataBindingComponent {
+
+}
