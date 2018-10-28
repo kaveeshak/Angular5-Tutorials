d0332e2c3961986bfe5b053d422d0dea3d61c1c3
Venura K Komangodage <kaveeshak@gmail.com>
2018-10-27 20:10:19 -0400

    Event binding.

--- ClientApplication/src/app/dataBinding/dataBinding.component.ts
+++ ClientApplication/src/app/dataBinding/dataBinding.component.ts
@@ -8,9 +8,16 @@ export class DataBindingComponent {
 
     age: number = 25;
     buttonClass = "btn btn-success";
+    incrementNumber = 0;
 
     constructor() {
         
         setTimeout(() => {this.buttonClass = "btn btn-danger";}, 5000)
+    }
+
+    onIncrementButtonClick(eventInfo: Event) {
+
+        this.incrementNumber += 1;
+        console.log(eventInfo);
     }
 }
