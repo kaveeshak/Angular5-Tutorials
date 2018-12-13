7cc362e799afb488fbddd283c1239308538f3a8c
Venura K Komangodage <kaveeshak@gmail.com>
2018-12-12 22:35:54 -0500

    Samples of structural and attribute directives.

--- ClientApplication/src/app/app.component.ts
+++ ClientApplication/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Tutorials';
+  title = 'Angular Tutorials 101';
   inputText = '';
 }
