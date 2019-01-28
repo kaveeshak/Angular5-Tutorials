a51a5ed2d5ac091c86dfd62b54a8f0ad03558da7
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2019-01-27 20:46:51 -0500

    Printing a list of recipes.

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Cake", "Bake an easy cake.", "https://tastesbetterfromscratch.com/wp-content/uploads/2010/06/Hersheys-Perfectly-Chocolate-Chocolate-Cake-13.jpg"),
+    new Recipe("Butter Chicken", "20 minutes butter chicken.", "https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto,w_600,h_750,c_fit,fl_strip_profile/https://s3.amazonaws.com/pixtruder/original_images/742cdd4738d4d0771cf05b715f7dbad3630e39e0")
+  ];
 
   constructor() { }
 
